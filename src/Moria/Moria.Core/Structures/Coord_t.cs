0b4ebce78b2fd8ee4cee19ab26b5c311005675a6
Maarten Oosterhoff <maarten.oosterhoff@gmail.com>
2020-04-27 13:21:49 +0200

    🐛 Adds debug helper.

--- src/Moria/Moria.Core/Structures/Coord_t.cs
+++ src/Moria/Moria.Core/Structures/Coord_t.cs
@@ -1,4 +1,6 @@
-﻿namespace Moria.Core.Structures
+﻿using System.Diagnostics;
+
+namespace Moria.Core.Structures
 {
     public class Coord_t
     {
@@ -6,13 +8,41 @@
         {
         }
 
-        public Coord_t(int x, int y)
+        public Coord_t(int y, int x)
         {
             this.x = x;
             this.y = y;
         }
 
-        public int x { get; set; }
-        public int y { get; set; }
+        private int _x;
+        public int x
+        {
+            get => _x;
+            set
+            {
+                if (value > 80)
+                {
+                    Debugger.Break();
+                }
+
+                _x = value;
+            }
+        }
+
+        private int _y;
+
+        public int y
+        {
+            get => _y;
+            set
+            {
+                if (value > 23)
+                {
+                    Debugger.Break();
+                }
+
+                _y = value;
+            }
+        }
     }
 }
