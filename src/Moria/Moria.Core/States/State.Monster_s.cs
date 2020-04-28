8e0f67228e4c4e674ef78ecc7c64db6a395c21bd
Maarten Oosterhoff <maarten.oosterhoff@gmail.com>
2020-04-28 12:36:36 +0200

    🐛 Fixes blank-monster bug.

--- src/Moria/Moria.Core/States/State.Monster_s.cs
+++ src/Moria/Moria.Core/States/State.Monster_s.cs
@@ -11,7 +11,7 @@ namespace Moria.Core.States
         public Monster_t[] monsters { get; } = ArrayInitializer.Initialize<Monster_t>(MON_TOTAL_ALLOCATIONS);
         public int[] monster_levels { get; } = new int[MON_MAX_LEVELS + 1];
 
-        public Monster_t blank_monster { get; } = new Monster_t();
+        public Monster_t blank_monster => new Monster_t();
         public int next_free_monster_id { get; set; }
         public int monster_multiply_total { get; set; }
     }
